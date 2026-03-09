9874ab03ae9ec4718b33bdadc280dfce388eb827
Vitinhoorei <vitorlo2006eduardo@gmail.com>
2026-03-09 08:51:09 -0300

    Fix: New adjustments

--- src/config.py
+++ src/config.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 import os
 from dotenv import load_dotenv
 
+IA_API_KEY = "FDiPWKBl7PT5ZargE8Gj7C05qnOoeQGB"
 IA_BASE_URL = "https://apihub.weg.net/external/fb8df9df-d49b-42b3-8da0-794e175cab44/v1/chat_genai"
 
 # carrega .env (se existir)
@@ -15,3 +16,4 @@ class AppConfig:
     """
     evidence_dir: str = os.getenv("EVIDENCE_DIR", "C:/Users/veleoterio/Downloads/testes-automação/evidence")
     output_dir: str = os.getenv("OUTPUT_DIR", "data/output")
+    
